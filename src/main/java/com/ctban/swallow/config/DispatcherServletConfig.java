948959da24e0cb11571a190d622af5345ae94b21
guojie <2874267468@qq.com>
2018-07-02 18:34:24 +0800

    提交bug

--- src/main/java/com/ctban/swallow/config/DispatcherServletConfig.java
+++ src/main/java/com/ctban/swallow/config/DispatcherServletConfig.java
@@ -39,11 +39,6 @@ public class DispatcherServletConfig extends WebMvcConfigurerAdapter {
         configurer.ignoreAcceptHeader(true);
     }
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-    }
-
 
     @Bean
     public FastJsonConfig fastJsonConfig() {
@@ -64,25 +59,6 @@ public class DispatcherServletConfig extends WebMvcConfigurerAdapter {
         fastJsonHttpMessageConverter.setSupportedMediaTypes(supportedMediaTypes);
         return fastJsonHttpMessageConverter;
     }
-
-
-//    @Bean
-//    public HttpMessageConverters  fastJsonHttpMessageConverters() {
-//        //1、定义一个convert转换消息的对象
-//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-//        //2、添加fastjson的配置信息
-//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-//        //2-1 处理中文乱码问题
-//        List<MediaType> fastMediaTypes = new ArrayList<>();
-//        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-//        fastConverter.setSupportedMediaTypes(fastMediaTypes);
-//        //3、在convert中添加配置信息
-//        fastConverter.setFastJsonConfig(fastJsonConfig);
-//        //4、将convert添加到converters中
-//        HttpMessageConverter<?> converter = fastConverter;
-//        return new HttpMessageConverters(converter);
-//    }
 
 
     @Bean
